56a149a7ed262840b5682a1ed5fd4b9fd93870c3
johnygnwst <92718963+johnygnwst@users.noreply.github.com>
2024-07-11 16:38:51 +0300

    Update and rename Timeslot.java to Timeslot.py

---
+++ GeneticProject/src/main/java/Timeslot.py
@@ -0,0 +1,10 @@
+class Timeslot:
+    def __init__(self, timeslotId, timeslot):
+        self.timeslotId = timeslotId
+        self.timeslot = timeslot
+
+    def getTimeslotId(self):
+        return self.timeslotId
+
+    def getTimeslot(self):
+        return self.timeslot
